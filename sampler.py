Actor model prenorm bug fix, added reoptimization rewards, added avgdualbound stats normalization.

sampler.py
@@ -33,6 +33,10 @@ class ActorSampler(mp.Process, pyscipopt.Branchrule):
         self._sample_count = 0
         self._benchmark = {}
         self._logger = None
+        self._reward = None
+        self._return = None
+        self._reoptimization_count = None
+        self._nb_steps = None
 
     def run(self):
         self.configure_logger()
@@ -59,31 +63,37 @@ class ActorSampler(mp.Process, pyscipopt.Branchrule):
                 model.readProblem(instance_path)
                 scip_utilities.init_scip_params(model, seed=self.seed)
 
-                recorder = SolvingStatsRecorder()
+                recorder = SolvingStatsRecorder(sampler=self)
                 model.includeEventhdlr(recorder, "SolvingStatsRecorder", "")
                 model.includeBranchrule(branchrule=self,
                     name="My branching rule", desc="",
                     priority=666666, maxdepth=-1, maxbounddist=1)
+                self._reward = NbNodesRewards(model)
+                self._return = 0.0
+                self._nb_steps = 0
 
                 # DEBUG
                 self._logger.info(f"Solving {instance_path}")
                 print(f"{self.name}: solving {instance_path}")
                 model.optimize()
-                self.save_results(model, recorder, instance_path, solving_stats_output_dir)
+                if self._nb_steps > 0:
+                    self._return += self._reward()
+                    self.save_results(model, recorder, instance_path, solving_stats_output_dir)
                 model.freeProb()
             self._logger.info(f"Done!")
         except Exception as exception:
             info = type(exception), exception, exception.__traceback__
             self._logger.info(''.join(traceback.format_exception(*info, limit=5)))
             raise exception
-    
-    def branchinit(self):
+        
+    def branchinitsol(self):
         self.state_buffer = {}
     
     def branchexeclp(self, allowaddcons):
-        # DEBUG
-        if psutil.virtual_memory().percent > 90:
-            self._logger.info(f"    Alert, current memory usage {psutil.virtual_memory().percent} > 90%. ***")
+        self._nb_steps += 1
+        previous_reward = self._reward()
+        if previous_reward is not None:
+            self._return += previous_reward
         
         state = scip_utilities.extract_state(self.model, self.state_buffer)
         # convert state to tensors
@@ -96,7 +106,7 @@ class ActorSampler(mp.Process, pyscipopt.Branchrule):
         )
         nb_constraints = tf.convert_to_tensor([c['values'].shape[0]], dtype=tf.int32)
         nb_variables = tf.convert_to_tensor([v['values'].shape[0]], dtype=tf.int32)
-        var_logits = self.actor((*state, nb_constraints, nb_variables)).numpy().squeeze(0)
+        var_logits = self.actor((*state, nb_constraints, nb_variables), tf.convert_to_tensor(False)).numpy().squeeze(0)
 
         candidate_vars, *_ = self.model.getLPBranchCands()
         candidate_mask = [var.getCol().getLPPos() for var in candidate_vars]
@@ -113,7 +123,8 @@ class ActorSampler(mp.Process, pyscipopt.Branchrule):
     def save_results(self, model, recorder, instance_path, solving_stats_output_dir):
         # Save benchmark
         if instance_path not in self._benchmark:
-            self._benchmark[instance_path] = {'nb_nodes': [], 'nb_lp_iterations': [], 'solving_time': []}
+            self._benchmark[instance_path] = {'return': [], 'nb_nodes': [], 'nb_lp_iterations': [], 'solving_time': []}
+        self._benchmark[instance_path]['return'].append(self._return)
         self._benchmark[instance_path]['nb_nodes'].append(model.getNNodes())
         self._benchmark[instance_path]['nb_lp_iterations'].append(model.getNLPIterations())
         self._benchmark[instance_path]['solving_time'].append(model.getSolvingTime())
@@ -126,9 +137,11 @@ class ActorSampler(mp.Process, pyscipopt.Branchrule):
             subsample_ends = np.random.choice(np.arange(1, len(recorder.stats)+1), nb_subsamples, replace=False).tolist()
             for subsample_end in subsample_ends:
                 subsample_stats = scip_utilities.pack_solving_stats(recorder.stats[:subsample_end])
+                return_left = self._return - recorder.return_[subsample_end-1]
                 nb_nodes_left = model.getNNodes() - recorder.nb_nodes[subsample_end-1]
                 nb_lp_iterations_left = model.getNLPIterations() - recorder.nb_lp_iterations[subsample_end-1]
                 solving_time_left = model.getSolvingTime() - recorder.solving_time[subsample_end-1]
+                
                 if self._sample_count < self.nb_solving_stats_samples:
                     self._sample_count += 1
                     sample_path = solving_stats_output_dir/f"sample_{self._sample_count-1}.pkl"
@@ -136,10 +149,14 @@ class ActorSampler(mp.Process, pyscipopt.Branchrule):
                         self._logger.info(f"Saving {sample_path}")
                     with gzip.open(str(sample_path), 'wb') as file:
                         pickle.dump({'solving_stats': subsample_stats, 
+                                     'return_left': return_left, 
                                      'nb_nodes_left': nb_nodes_left, 
                                      'nb_lp_iterations_left': nb_lp_iterations_left, 
                                      'solving_time_left': solving_time_left, 
                                      'instance_path': instance_path}, file)
+    
+    def branchexitsol(self):
+        self._reward.snapshot_reward()
 
     def load_actor(self):
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
@@ -171,8 +188,11 @@ class SolvingStatsRecorder(pyscipopt.Eventhdlr):
     """
     A SCIP event handler that records solving stats
     """
-    def __init__(self):
+    def __init__(self, sampler):
+        self.sampler = sampler
+        
         self.stats = []
+        self.return_ = []
         self.nb_nodes = []
         self.nb_lp_iterations = []
         self.solving_time = []
@@ -190,6 +210,7 @@ class SolvingStatsRecorder(pyscipopt.Eventhdlr):
     def eventexec(self, event):
         if len(self.stats) < self.model.getNNodes():
             self.stats.append(self.model.getSolvingStats())
+            self.return_.append(float(self.sampler._return))
             self.nb_nodes.append(self.model.getNNodes())
             self.nb_lp_iterations.append(self.model.getNLPIterations())
             self.solving_time.append(self.model.getSolvingTime())
@@ -201,8 +222,24 @@ class Message(enum.Enum):
     STOP = enum.auto()
 
 
-# class InstanceManager:
-#     def __init__(self):
+class NbNodesRewards:
+    def __init__(self, model):
+        self.model = model
+        self.previous_nb_nodes = None
+        self.reward = None
     
-#     def acquire(instance):
-        
+    def __call__(self):
+        if self.reward is None:
+            self.snapshot_reward()
+        reward = self.reward
+        self.reward = None
+        return reward
+    
+    def snapshot_reward(self):
+        nb_nodes = self.model.getNNodes()
+        if self.previous_nb_nodes is not None:
+            self.reward = float(self.previous_nb_nodes - nb_nodes)
+            self.previous_nb_nodes = nb_nodes
+        else:
+            self.reward = None
+            self.previous_nb_nodes = self.model.getNNodes()

train.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     tf.set_random_seed(seed=0)
     rng = np.random.RandomState(0)
 
-    data_folder = Path('data/bnb_size_prediction/baseline')
+    data_folder = Path('data/bnb_size_prediction/setcover')
     train_folder = data_folder/"train_500r_1000c_0.05d"
     test_folder  = data_folder/"test_500r_1000c_0.05d"
     output_folder = Path('results/setcover')
@@ -103,12 +103,14 @@ if __name__ == "__main__":
 
     feature_means, feature_stds = get_feature_stats(train_data, train_folder)
 
-    lr_start = 1e-3
-    lr_end = 1e-3
-    max_epochs = 200
+#     lr_start = 1e-3
+#     lr_end = 1e-3
 
-    def learning_rate(episode):
-        return (lr_start-lr_end) / np.e ** episode + lr_end
+#     def learning_rate(episode):
+#         return (lr_start-lr_end) / np.e ** episode + lr_end
+    lr = 1e-3
+    patience = 20
+    max_epochs = 500
 
     model = Model(feature_means, feature_stds)
     optimizer = tf.train.AdamOptimizer(lambda: lr)
@@ -132,7 +134,7 @@ if __name__ == "__main__":
             response_scales = tf.cast(tf.stack(response_scales, axis=0), tf.float32)
             responses = (responses - response_centers) / response_scales
 
-        lr = learning_rate(epoch)
+#         lr = learning_rate(epoch)
         with tf.GradientTape() as tape:
             predictions = model(features)
             loss = tf.reduce_mean(tf.square(predictions - responses))
@@ -166,3 +168,11 @@ if __name__ == "__main__":
             best_test_loss = test_loss
             print(" * New best test loss *")
             model.save_state(output_folder/"best_params.pkl")
+            patience = 20
+        else:
+            patience -= 1
+            if patience == 0:
+                lr /= 2
+                print(f" * Reduced learning rate to {lr}")
+                patience = 20
+                