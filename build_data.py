17a52b77f6bc6ee40947bbd7630a674014060318
Didier Chetelat <didier.chetelat@polymtl.ca>
2019-11-08 13:52:21 -0500

    Phase 1 (new scip)

--- build_data.py
+++ build_data.py
@@ -71,7 +71,7 @@ actor_samplers = [ActorSampler(parameters_path, nb_solving_stats_samples=int(NB_
 for actor_sampler in actor_samplers:
     actor_sampler.start()
 
-train_output_path = Path("data/classic_bnb_size_prediction")/train_folder
+train_output_path = Path("data/newscip_bnb_size_prediction")/train_folder
 for count, instance_path in enumerate(train_instances):
     if count > NB_TRAIN_SAMPLES/(NB_SAMPLERS*10):
         break
@@ -95,7 +95,7 @@ actor_samplers = [ActorSampler(parameters_path, nb_solving_stats_samples=int(NB_
 for actor_sampler in actor_samplers:
     actor_sampler.start()
 
-valid_output_path = Path("data/classic_bnb_size_prediction")/valid_folder
+valid_output_path = Path("data/newscip_bnb_size_prediction")/valid_folder
 for count, instance_path in enumerate(valid_instances):
     if count > NB_VALID_SAMPLES/(NB_SAMPLERS*10):
         break
--- train.py
+++ train.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     tf.set_random_seed(seed=0)
     rng = np.random.RandomState(0)
     
-    data_folder = Path('data/classic_bnb_size_prediction/cauctions')
+    data_folder = Path('data/newscip_bnb_size_prediction/cauctions')
     train_folder = data_folder/"train_100_500"
     valid_folder  = data_folder/"valid_100_500"
     output_folder = Path('results/cauctions')
